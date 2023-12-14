44bba2573bd4ec37b63c31a8d85d1769b2869787
FaissalMarzouki <fayssalmarzoki555@gmail.com>
2023-12-14 21:15:17 +0100

    saving src code

---
+++ src/main/java/com/Mecatronique/Faissal/BackendWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.Mecatronique.Faissal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebsiteApplication.class, args);
+	}
+
+}
